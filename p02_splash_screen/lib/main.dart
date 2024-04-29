5402c2bebd25d45f7d64ee1dea3f8373d90c1a85
birdcagedout <birdcagedout@gmail.com>
2024-04-29 19:47:43 +0900

    2024029_work

--- p02_splash_screen/lib/main.dart
+++ p02_splash_screen/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,19 +16,24 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF335CB0),   // Hex값 입력할 때는 Color(값), FF는 Alpha
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 32.0
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('asset/img/logo.png'),
-            SizedBox(height: 28.0,),
-            CircularProgressIndicator(color: Colors.white,),
-          ],
-        ),
+      // backgroundColor: Color(0xFF335CB0),   // Hex값 입력할 때는 Color(값), FF는 Alpha
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(padding: EdgeInsets.symmetric(horizontal: 80.0), child: Image.asset('asset/img/grpay2.png')),
+          SizedBox(height: 5.0),
+          Text("급 량 페 이", style: TextStyle(fontSize: 60, fontWeight: FontWeight.w900,),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("QR Scanner ", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w900,),),
+              Text("ver.0.1b", style: TextStyle(fontSize: 20.0, color: Colors.green),),
+            ],
+          ),
+          SizedBox(height: 15.0),
+          CircularProgressIndicator(color: Color(0xFFFFC400),),
+        ],
       )
     );
   }
---
+++ p03_row_column/android/app/src/main/kotlin/com/birdcagedout/p03_row_column/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.birdcagedout.p03_row_column
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
